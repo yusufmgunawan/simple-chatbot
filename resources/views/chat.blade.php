5dda6abf109756930942bc5b5a2de2dadf2dd627
Yusuf Gunawan <yusufmg27@gmail.com>
2025-07-18 10:24:15 +0700

    test

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Chatbot</title>
+    <title>Simple Chatbot | TEST</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;700&display=swap" rel="stylesheet">
     <style>
